714b15f60c8596b9d64971a5ae5f8c071308fc5a
Pedro <pedro.souza2@estudante.ufla.br>
2021-04-11 12:07:58 -0300

    Developed DTO for amount to apply or withdraw

---
+++ src/main/java/one/digitalinnovation/investment/dto/AmountDTO.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.investment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AmountDTO {
+
+    @NotNull
+    @Positive
+    public Double amount;
+
+}
