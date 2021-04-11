7f47327ce9e4c6b47001aa2fd5c3c1dcb9fedfd7
Pedro <pedro.souza2@estudante.ufla.br>
2021-04-11 01:24:32 -0300

    Developed service tests for investment creation

---
+++ src/test/java/one/digitalinnovation/investment/controller/InvestmentControllerTest.java
@@ -0,0 +1,78 @@
+package one.digitalinnovation.investment.controller;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import one.digitalinnovation.investment.builder.InvestmentDTOBuilder;
+import one.digitalinnovation.investment.dto.InvestmentDTO;
+import one.digitalinnovation.investment.service.InvestmentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static one.digitalinnovation.investment.utils.JsonConvertionUtils.asJsonString;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class InvestmentControllerTest {
+
+    private static final String INVESTMENT_API_URL_PATH = "/api/v1/investments";
+    private static final long VALID_INVESTMENT_ID = 1L;
+    private static final long INVALID_INVESTMENT_ID = 2L;
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private InvestmentService investmentService;
+
+    @InjectMocks
+    private InvestmentController investmentController;
+
+    @BeforeEach
+    void setUp() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModules(new JavaTimeModule());
+
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new
+                MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(investmentController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setMessageConverters(mappingJackson2HttpMessageConverter)
+                .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenAnInvestmentIsCreated() throws Exception {
+        // given
+        InvestmentDTO investmentDTO = InvestmentDTOBuilder.builder().id(null).build().toInvestmentDTO();
+
+        // when
+        when(investmentService.createInvestment(investmentDTO)).thenReturn(investmentDTO);
+
+        // then
+        mockMvc.perform(post(INVESTMENT_API_URL_PATH)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(asJsonString(investmentDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(investmentDTO.getName())))
+                .andExpect(jsonPath("$.initialDate", is(investmentDTO.getInitialDate().toString())))
+                .andExpect(jsonPath("$.expirationDate", is(investmentDTO.getExpirationDate().toString())));
+    }
+}
---
+++ src/test/java/one/digitalinnovation/investment/service/InvestmentServiceTest.java
@@ -0,0 +1,86 @@
+package one.digitalinnovation.investment.service;
+
+import one.digitalinnovation.investment.builder.InvestmentDTOBuilder;
+import one.digitalinnovation.investment.dto.InvestmentDTO;
+import one.digitalinnovation.investment.entity.Investment;
+import one.digitalinnovation.investment.exception.InvestmentAlreadyRegisteredException;
+import one.digitalinnovation.investment.exception.InvestmentInvalidExpirationDateException;
+import one.digitalinnovation.investment.mapper.InvestmentMapper;
+import one.digitalinnovation.investment.repository.InvestmentRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class InvestmentServiceTest {
+
+    private static final long INVALID_INVESTMENT_ID = 1L;
+
+    @Mock
+    private InvestmentRepository investmentRepository;
+
+    private final InvestmentMapper investmentMapper = InvestmentMapper.INSTANCE;
+
+    @InjectMocks
+    private InvestmentService investmentService;
+
+    @Test
+    void whenInvestmentInformedThenItShouldBeCreated() throws InvestmentAlreadyRegisteredException, InvestmentInvalidExpirationDateException {
+        // given
+        InvestmentDTO expectedInvestmentDTO = InvestmentDTOBuilder.builder().build().toInvestmentDTO();
+        Investment expectedSavedInvestment = investmentMapper.toModel(expectedInvestmentDTO);
+
+        // when
+        when(investmentRepository.findByName(expectedInvestmentDTO.getName())).thenReturn(Optional.empty());
+        when(investmentRepository.save(expectedSavedInvestment)).thenReturn(expectedSavedInvestment);
+
+        // then
+        InvestmentDTO createdInvestmentDTO = investmentService.createInvestment(expectedInvestmentDTO);
+
+        assertThat(createdInvestmentDTO.getId(), is(equalTo(expectedInvestmentDTO.getId())));
+        assertThat(createdInvestmentDTO.getName(), is(equalTo(expectedInvestmentDTO.getName())));
+        assertThat(createdInvestmentDTO.getValue(), is(equalTo(expectedInvestmentDTO.getValue())));
+        assertThat(createdInvestmentDTO.getInitialDate(), is(lessThan(createdInvestmentDTO.getExpirationDate())));
+    }
+
+    @Test
+    void whenAlreadyRegisteredInvestmentInformedThanAnExceptionShouldBeThrown() throws InvestmentAlreadyRegisteredException {
+        // given
+        InvestmentDTO expectedInvestmentDTO = InvestmentDTOBuilder.builder().build().toInvestmentDTO();
+        Investment duplicatedInvestment = investmentMapper.toModel(expectedInvestmentDTO);
+
+        // when
+        when(investmentRepository.findByName(expectedInvestmentDTO.getName())).thenReturn(Optional.of(duplicatedInvestment));
+
+        // then
+        assertThrows(InvestmentAlreadyRegisteredException.class, () -> investmentService.createInvestment(expectedInvestmentDTO));
+    }
+
+    @Test
+    void whenInvestmentWithInitialDateGreaterThanExpirationDateInformedThanExceptionShouldBeThrown() throws InvestmentAlreadyRegisteredException, InvestmentInvalidExpirationDateException {
+        // given
+        InvestmentDTO expectedInvestmentDTO = InvestmentDTOBuilder.builder()
+                .initialDate(LocalDate.of(2000, 1, 1))
+                .expirationDate(LocalDate.of(1970, 1, 1))
+                .build().toInvestmentDTO();
+
+        // then
+        assertThrows(InvestmentInvalidExpirationDateException.class, () -> investmentService.createInvestment(expectedInvestmentDTO));
+    }
+
+}
